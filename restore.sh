1f6dbe267b3025f6989172cec38b488330632203
andrewmayer1997 <51457126+andrewmayer1997@users.noreply.github.com>
2021-10-14 11:53:59 -0700

    Update restore.sh

--- restore.sh
+++ restore.sh
@@ -6,20 +6,22 @@ LOG="/usr/local/etc/${SERIAL}-${DATE}"
 
 echo "START...\n" > $LOG;
 
-/bin/python2 /usr/local/bin/keyhunter.py /dev/$NAME > $LOG;
-
+beep -f 1000 -l 400
 echo "1" | tee -a /sys/class/leds/input*::capslock/brightness > /dev/null;
 sleep 3;
 echo "0" | tee -a /sys/class/leds/input*::capslock/brightness > /dev/null;
+
+/bin/python2 /usr/local/bin/keyhunter.py /dev/$NAME > $LOG;
 
 LINES=$(wc -l < $LOG)
 
 if (($LINES > 1)); then
 	mv $LOG $LOG.true
-	
+	beep -f 5000 -l 400 -r 4
 	echo "1" | tee -a /sys/class/leds/input*::numlock/brightness > /dev/null;
 	sleep 3;
 	echo "0" | tee -a /sys/class/leds/input*::numlock/brightness > /dev/null;
 else
 	mv $LOG $LOG.false
+	beep -f 1000 -l 400
 fi
